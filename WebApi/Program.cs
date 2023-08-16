6ad012f632a26b64defe9f288d93668a19b74756
徐文帅 <1669228230@qq.com>
2023-08-16 11:54:30 +0800

    fix:格式化

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -20,7 +20,9 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
     #region 通过模块化注册接口和实现类
+
     containerBuilder.RegisterModule(new AutofacModuleRegister());
+
     #endregion
 
     #region sqlSugar注入
@@ -43,28 +45,31 @@ builder.Services.AddAutoMapper(typeof(AutoMapperConfigs));
 
 builder.Services.Configure<JWTTokenOptions>(builder.Configuration.GetSection("JWTTokenOptions"));
 
-#region jwt校验 
+#region jwt校验
+
 {
     //第二步，增加鉴权逻辑
-    JWTTokenOptions tokenOptions = new JWTTokenOptions();
+    var tokenOptions = new JWTTokenOptions();
     builder.Configuration.Bind("JWTTokenOptions", tokenOptions);
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)//Scheme
-        .AddJwtBearer(options =>  //这里是配置的鉴权的逻辑
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //Scheme
+        .AddJwtBearer(options => //这里是配置的鉴权的逻辑
         {
             options.TokenValidationParameters = new TokenValidationParameters
             {
                 //JWT有一些默认的属性，就是给鉴权时就可以筛选了
-                ValidateIssuer = true,//是否验证Issuer
-                ValidateAudience = true,//是否验证Audience
-                ValidateLifetime = true,//是否验证失效时间
-                ValidateIssuerSigningKey = true,//是否验证SecurityKey
-                ValidAudience = tokenOptions.Audience,//
-                ClockSkew = TimeSpan.FromSeconds(0),//设置token过期后多久失效，默认过期后300秒内仍有效
-                ValidIssuer = tokenOptions.Issuer,//Issuer，这两项和前面签发jwt的设置一致
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey))//拿到SecurityKey 
+                ValidateIssuer = true, //是否验证Issuer
+                ValidateAudience = true, //是否验证Audience
+                ValidateLifetime = true, //是否验证失效时间
+                ValidateIssuerSigningKey = true, //是否验证SecurityKey
+                ValidAudience = tokenOptions.Audience, //
+                ClockSkew = TimeSpan.FromSeconds(0), //设置token过期后多久失效，默认过期后300秒内仍有效
+                ValidIssuer = tokenOptions.Issuer, //Issuer，这两项和前面签发jwt的设置一致
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey)) //拿到SecurityKey 
             };
         });
 }
+
 #endregion
 
 var app = builder.Build();
