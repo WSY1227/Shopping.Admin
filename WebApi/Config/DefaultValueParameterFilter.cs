6c5700f96b0ce1afce59d3f48676d50e1b45d480
徐文帅 <1669228230@qq.com>
2023-08-22 09:49:45 +0800

    fix:swagger传参特性

---
+++ WebApi/Config/DefaultValueParameterFilter.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WebApi.Config;
+
+public class DefaultValueParameterFilter: ISchemaFilter
+{
+    public void Apply(OpenApiSchema? schema, SchemaFilterContext context)
+    {
+        if (schema?.Properties == null || context.Type == null) return;
+        foreach (var property in schema.Properties)
+        {
+            property.Value.Default = property.Value.Type switch
+            {
+                // 按照数据类型设置默认值
+                "string" when property.Value.Default == null => new OpenApiString(""),
+                "integer" when property.Value.Default == null => new OpenApiInteger(0),
+                "number" when property.Value.Default == null => new OpenApiDouble(0),
+                _ => property.Key switch
+                {
+                    "pageIndex" => new OpenApiInteger(1),
+                    "pageSize" => new OpenApiInteger(10),
+                    _ => property.Value.Default
+                }
+            };
+            
+            // 通过特性来实现
+            var defaultAttribute = context.ParameterInfo?.GetCustomAttribute<DefaultValueAttribute>();
+            
+            if (defaultAttribute != null)
+            {
+                property.Value.Example = (IOpenApiAny)defaultAttribute.Value;
+                
+            }
+            
+        }
+    }
+}
